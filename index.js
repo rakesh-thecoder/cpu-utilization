675f3cad3a5f1c1e1fff97139d9721217434bad8
r-rakesh28 <rebbavarapurakesh@gmail.com>
2021-10-07 18:53:18 +0530

    led blick function added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
 const ncpu = require("os").cpus().length;
+const Gpio = require('onoff').Gpio;
+
+const red = new Gpio(4, 'out'); 
+const green = new Gpio(4, 'out'); 
+
 let previousTime = new Date().getTime();
 let previousUsage = process.cpuUsage();
 let lastUsage;
@@ -18,4 +23,13 @@ setInterval(() => {
     previousTime = currentTime;
 
     console.log(usage);
+
+    if(usage > 0.4){
+    	red.writeSync(1);
+    	green.writeSync(0);
+    }
+    else{
+    	red.writeSync(0);
+    	green.writeSync(1);
+    }
 }, 1000);
